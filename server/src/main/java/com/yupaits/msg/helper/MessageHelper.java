9675f5a9f0c41d65ce395f5335dcccb375bd97c2
tangsheng <ts@posun.cn>
2018-11-20 11:52:09 +0800

    完成根据模板格式化消息内容的功能

--- server/src/main/java/com/yupaits/msg/helper/MessageHelper.java
+++ server/src/main/java/com/yupaits/msg/helper/MessageHelper.java
@@ -1,13 +1,18 @@
 package com.yupaits.msg.helper;
 
+import com.alibaba.fastjson.JSONObject;
 import com.yupaits.commons.utils.ValidateUtils;
 import com.yupaits.msg.entity.Template;
 import com.yupaits.msg.service.ITemplateService;
 import com.yupaits.msg.vo.MessageVO;
 import com.yupaits.msg.vo.TemplateVO;
+import org.apache.commons.collections4.CollectionUtils;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+
+import java.util.List;
 
 /**
  * @author yupaits
@@ -34,20 +39,30 @@ public class MessageHelper {
             TemplateVO templateVO = new TemplateVO();
             BeanUtils.copyProperties(template, templateVO);
             templateVO.setFillFields(template.getFillFields());
-            return parseMessageContent(templateVO, messageVO);
+            return messageContentFormat(templateVO, messageVO);
         } else {
             return messageVO.getContent();
         }
     }
 
     /**
-     * 根据模板解析获取消息内容
+     * 根据模板和参数格式化消息内容
      * @param templateVO 消息模板
      * @param messageVO 消息对象
-     * @return 解析后的消息内容
+     * @return 格式化后的消息内容
      */
-    private String parseMessageContent(TemplateVO templateVO, MessageVO messageVO) {
-        //TODO 解析逻辑
-        return "";
+    private String messageContentFormat(TemplateVO templateVO, MessageVO messageVO) {
+        String content = templateVO.getTemplatePattern();
+        List<String> fillFields = templateVO.getFillFields();
+        JSONObject payload = messageVO.getPayload();
+        if (CollectionUtils.isNotEmpty(fillFields)) {
+            for (String field : fillFields) {
+                if (StringUtils.isBlank(field)) {
+                    continue;
+                }
+                content = content.replace("{{" + field + "}}", payload.getString(field));
+            }
+        }
+        return content;
     }
 }
